87b661beac5b69d43dba40dfc0f8e3eba282b7b6
MbarekAmehry <mbarek.amehry@gmail.com>
2022-01-31 12:05:15 +0100

    eslint: allow double quotes

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -2,18 +2,17 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
-    parser: '@babel/eslint-parser',
-    requireConfigFile: false
+    parser: "@babel/eslint-parser",
+    requireConfigFile: false,
   },
-  extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended',
-  ],
-  plugins: [
-  ],
+  extends: ["@nuxtjs", "plugin:nuxt/recommended"],
+  plugins: [],
   // add your custom rules here
-  rules: {}
-}
+  rules: {
+    "vue/multi-word-component-names": true,
+    quotes: [2, "single", { avoidEscape: true }],
+  },
+};
